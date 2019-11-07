db94f6968990fc53b70092b7a06eeecd977cc09c
Adam Boström <abm19002@student.mdh.se>
2019-11-07 14:45:54 +0100

    commiting a project with double pointers to Github

---
+++ anotherproject/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+char * s1 = "Emilia";			// variabeln s1 är en variabel som går att ändra, och vid
+								// start tilldelas värdet av adressen till 'E’.
+
+char s2[] = "Roger";			// värdet på s2 känt vid compile time. s2 är konstant, dvs
+								// ingen variabel som går att ändra. Är adressen till ‘R’.
+int main()
+{
+	printf("Kul med pekare\n");
+
+	char **pp, *p = s1;
+	pp = &p;
+	printf("p = %s\n", p);		// p == s1, så ekvivalent med att skicka s1 som argument.
+	printf("*pp = %s\n", *pp);
+
+
+	*pp = s2;					// *pp ekvivalent med p. Ändrar p till s2
+	printf("p = %s\n", p);
+
+
+	*p = 'T';					// (p == s2). *s2 ekv. med s2[0]. Ändrar s2[0] till ’T’
+	**pp = 'J';					// *pp ekv med p. p==s2. -> *s2 = ’J’. Ändrar s2[0] till 'J’
+	*(*pp + 2) = 'k';			// ändrar s2[2] till 'k’
+	printf("%s\n", p);
+
+
+	(*pp)[0] = 'P';				// *pp ekv med p (== s2). s2[0]. Ändrar s2[0] till 'P'
+	printf("%s\n", p);
+
+	system("pause");
+	return 0;
+}
+
